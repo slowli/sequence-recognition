287820f16527e61adf68edad73fb1426b03d0fe5
Alex Ostrovski <ostrovski.alex@gmail.com>
2015-01-24 18:28:07 +0200

    changed to inherit from SimpleSequenceSet

--- bio.seq/src/ua/kiev/icyb/bio/alg/EstimatesSet.java
+++ bio.seq/src/ua/kiev/icyb/bio/alg/EstimatesSet.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 import ua.kiev.icyb.bio.Sequence;
 import ua.kiev.icyb.bio.SequenceSet;
-import ua.kiev.icyb.bio.res.Messages;
+import ua.kiev.icyb.bio.SimpleSequenceSet;
 
 /**
  * Имплементация интерфейса {@link SequenceSet}, хранящая исключительно строки
@@ -15,18 +15,14 @@ import ua.kiev.icyb.bio.res.Messages;
  * Используется в алгоритмах распознавания скрытых последовательностей для хранения
  * результатов работы.
  */
-public final class EstimatesSet implements SequenceSet {
+class EstimatesSet extends SimpleSequenceSet {
 	
 	private static final long serialVersionUID = 1L;
 	
-	/**
-	 * Выборка, на которой будет работать алгоритм распознавания.
-	 */
-	private final SequenceSet baseSet;
+	public final SequenceSet baseSet;
 	
-	/** Последовательности скрытых состояний. */
-	private final byte[][] hidden;
-
+	private final byte[][] hidden; 
+	
 	/**
 	 * Создает контейнер для сохранения результатов работы алгоритма распознавания
 	 * скрытых последовательностей. 
@@ -35,102 +31,45 @@ public final class EstimatesSet implements SequenceSet {
 	 *    выборка, на которой будет работать алгоритм распознавания
 	 */
 	public EstimatesSet(SequenceSet set) {
-		baseSet = set;
-		hidden = new byte[set.length()][];
+		super(set.observedStates(), set.hiddenStates(), set.completeStates());
+		this.baseSet = set;
+		
+		this.hidden = new byte[set.length()][];
+		for (int i = 0; i < set.length(); i++) {
+			this.hidden[i] = new byte[set.observed(i).length];
+			this.doAdd(new Sequence(set.id(i), set.observed(i), null));
+		}
 	}
 
 	/**
 	 * Сохраняет последовательность скрытых состояний в этом наборк.
 	 * 
 	 * @param index
-	 *        индекс (с отсчетом от нуля) наблюдаемой последовательности,
-	 *        для которой найдены соответствующие скрытые состояния
+	 *    индекс (с отсчетом от нуля) наблюдаемой последовательности,
+	 *    для которой найдены соответствующие скрытые состояния
 	 * @param array
-	 *        последовательность скрытых состояний для сохранения
+	 *    последовательность скрытых состояний для сохранения
 	 */
 	public void put(int index, byte[] array) {
 		hidden[index] = array;
 	}
-
-	@Override
-	public int length() {
-		return hidden.length;
-	}
-
-	@Override
-	public String observedStates() {
-		return baseSet.observedStates();
-	}
-
-	@Override
-	public String hiddenStates() {
-		return baseSet.hiddenStates();
-	}
 	
-	@Override
-	public String completeStates() {
-		return baseSet.completeStates();
-	}
-
-	@Override
-	public byte[] observed(int index) {
-		return baseSet.observed(index);
-	}
-
 	@Override
 	public byte[] hidden(int index) {
 		return hidden[index];
 	}
 	
 	@Override
-	public String id(int index) {
-		return baseSet.id(index);
-	}
-
-	@Override
-	public Sequence get(int index) {
-		return new Sequence(this, index, id(index), observed(index), hidden(index));
-	}
-
-	
-	@Override
 	public void saveToFile(String fileName) throws IOException {
-		// TODO ???
+		// TODO writeObject?
 		BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
 		writer.write(hidden.length + "\n");
 		for (int i = 0; i < hidden.length; i++) {
-			writer.write(i + ":");
+			writer.write(this.id(i) + ": ");
 			for (int pos = 0; pos < hidden[i].length; pos++)
-				writer.write('0' + hidden[i][pos]);
+				writer.write(hiddenStates().charAt(hidden[i][pos]));
 			writer.write("\n");
 		}
 		writer.close();
-	}
-
-	/**
-	 * <strong>Операция не реализована.</strong>
-	 * <p>{@inheritDoc}
-	 * 
-	 * @throws UnsupportedOperationException всегда
-	 */
-	@Override
-	public SequenceSet filter(boolean[] selector) {
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * <strong>Операция не реализована.</strong>
-	 * <p>{@inheritDoc}
-	 * 
-	 * @throws UnsupportedOperationException всегда
-	 */
-	@Override
-	public SequenceSet filter(Filter filter) {
-		throw new UnsupportedOperationException();
-	}
-	
-	@Override
-	public String repr() {
-		return Messages.format("dataset.est", baseSet.repr());
 	}
 }
