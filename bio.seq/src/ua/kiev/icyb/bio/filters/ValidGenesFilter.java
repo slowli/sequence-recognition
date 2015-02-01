ec0057f16c4d049fe662f26d279f5d52636010b5
Alex Ostrovski <ostrovski.alex@gmail.com>
2015-02-01 23:25:41 +0200

    removed hard-coded stuff

--- bio.seq/src/ua/kiev/icyb/bio/filters/ValidGenesFilter.java
+++ bio.seq/src/ua/kiev/icyb/bio/filters/ValidGenesFilter.java
@@ -14,7 +14,7 @@ import ua.kiev.icyb.bio.SequenceSet;
  * 
  * <p>Кроме того, фильтр может проверять условие, касающееся интронов:
  * <ul>
- *   <li>каждый интрон начинается с нуклеотида <code>G</code> и заканчивается нуклеотидом <code>G</code>.
+ *   <li>каждый интрон начинается с нуклеотидов <code>GT</code> и заканчивается нуклеотидами <code>AG</code>.
  * </ul>
  */
 public class ValidGenesFilter implements SequenceSet.Filter {
@@ -34,30 +34,70 @@ public class ValidGenesFilter implements SequenceSet.Filter {
 		this.validateIntrons = validateIntrons;
 	}
 	
+	/**
+	 * Сравнивает подстроку последовательности с эталоном.
+	 * 
+	 * @param symbols
+	 *    последовательность состояний
+	 * @param sample
+	 *    эталонная строка состояний
+	 * @param start
+	 *    предполагаемое начало эталонной строки в последовательности
+	 * @return
+	 *    {@code true}, если эталон входит в строку состояний, начиная с заданной позиции {@code start}
+	 */
+	private boolean checkSubstring(byte[] symbols, byte[] sample, int start) {
+		for (int pos = 0; pos < sample.length; pos++) {
+			if (symbols[start + pos] != sample[pos]) return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Восстанавливает последовательность состояний по ее текстовому представлению.
+	 * 
+	 * @param text
+	 *    текстовое представление состояний
+	 * @param alphabet
+	 *    алфавит состояний
+	 * @return
+	 *   восстановленный массив состояний
+	 */
+	private byte[] getSymbols(String text, String alphabet) {
+		byte[] symbols = new byte[text.length()];
+		for (int i = 0; i < text.length(); i++) {
+			symbols[i] = (byte) alphabet.indexOf(text.charAt(i));
+		}
+		return symbols;
+	}
+	
 	@Override
 	public boolean eval(Sequence sequence) {
-		final byte[] observed = sequence.observed, hidden = sequence.hidden;
+		final byte[] observed = sequence.observed;
 		
-		boolean validStart = (observed[0] == 0) && (observed[1] == 3) && (observed[2] == 2);
+		final byte[] startSeq = getSymbols("ATG", sequence.set.observedStates());
+		final byte[] termSeq1 = getSymbols("TGA", sequence.set.observedStates()),
+			termSeq2 = getSymbols("TAG", sequence.set.observedStates()),
+			termSeq3 = getSymbols("TAA", sequence.set.observedStates());
+		
+		boolean validStart = checkSubstring(observed, startSeq, 0);
 		if (!validStart) return false;
 		
 		int len = observed.length;
-		boolean validEnd = ((observed[len - 3] == 3) && (observed[len - 2] == 0) && (observed[len - 1] == 0))
-				|| ((observed[len - 3] == 3) && (observed[len - 2] == 0) && (observed[len - 1] == 2))
-				|| ((observed[len - 3] == 3) && (observed[len - 2] == 2) && (observed[len - 1] == 0));
+		boolean validEnd = checkSubstring(observed, termSeq1, len - 3) 
+				|| checkSubstring(observed, termSeq2, len - 3)
+				|| checkSubstring(observed, termSeq3, len - 3);
 		if (!validEnd) return false;
 		
 		if (!validateIntrons) return true;
 		
-		for (int pos = 1; pos < hidden.length - 1; pos++) {
-			boolean intronStart = (hidden[pos - 1] == 0) && (hidden[pos] == 1);
-			boolean intronEnd = (hidden[pos - 1] == 1) && (hidden[pos] == 0);
-			
-			if (intronStart && (observed[pos] != 2)) {
-				return false;
-			}
-			if (intronEnd && (observed[pos - 1] != 2)) {
-				return false;
+		final byte[] intronStart = getSymbols("GT", sequence.set.observedStates()),
+				intronEnd = getSymbols("AG", sequence.set.observedStates());
+		
+		for (Sequence.Segment segment : sequence.segments()) {
+			if (segment.stateChar() == 'i') {
+				if (!checkSubstring(observed, intronStart, segment.start)) return false;
+				if (!checkSubstring(observed, intronEnd, segment.end - 1)) return false;
 			}
 		}
 		
