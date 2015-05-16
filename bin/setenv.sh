1e9acd3f05ddd90985d9eec1cb51431215eb2032
Alex Ostrovski <ostrovski.alex@gmail.com>
2015-05-16 20:59:37 +0300

    Added examples and converted local variables in scripts to lower case.

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -6,11 +6,11 @@
 
 # Root directory of the installation
 # Корневая директория установки.
-ROOT=`dirname $0`/..
+root=`dirname $0`/..
 
 # Version of the library
 # Версия библиотеки.
-LIBVER=0.9.1
+lib_version=0.10.0
 
 # Determines the running environment for commands.
 #   0 - use the stable build (JAR file)
@@ -27,14 +27,14 @@ JAVAOPTS="-Xms512m -Xmx4g"
 # Java classpath
 # Путь к модулям и классам Java.
 if [[ $DEBUG == "1" ]]; then
-    CLASSPATH=$ROOT/bio.seq/bin:$ROOT/lib/bytecode-1.8.1.jar:$ROOT/lib/core-1.8.1.jar:$CLASSPATH
+    CLASSPATH=$root/bio.seq/bin:$root/lib/bytecode-1.8.1.jar:$root/lib/core-1.8.1.jar:$CLASSPATH
 else
-    CLASSPATH=$ROOT/bin/bio.seq-$LIBVER-full.jar:$CLASSPATH
+    CLASSPATH=$root/bin/bio.seq-$lib_version-full.jar:$CLASSPATH
 fi
 
 ENV_FILE=./env.conf
 if [[ ! -f $ENV_FILE ]]; then
-	ENV_FILE=$ROOT/bin/env.conf
+	ENV_FILE=$root/bin/env.conf
 fi
 
 export CLASSPATH \
